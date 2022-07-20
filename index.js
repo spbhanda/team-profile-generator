ae99b2f04a3d29d8071776e9f9f2e018ea784313
spbhanda <spbhanda@gmail.com>
2022-07-19 21:33:53 -0500

    add questions

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const inquirer = require("inquirer");
-const addEmployee = require("./lib/addEmployee");
 const employee = require("./lib/Employee");
 const manager = require("./lib/Manager");
 const engineer = require("./lib/Engineer");
@@ -8,37 +7,35 @@ const intern = require("./lib/Intern");
 // Initialize
 
 function employees() {
-    addEmployee();
-    
-    
-
-   const questions = [
-      {
-         type: "text",
-         name: "title",
-         message: "What is your title?",
-      },
-
-      {
-         type: "text",
-         name: "name",
-         message: "What is your name?",
-      },
-      {
-         type: "text",
-         name: "office",
-         message: "What is your Office?",
-      },
-
-      {
-         type: "text",
-         name: "id",
-         message: "What is your ID?",
-      },
-   ];
-
-   inquirer.prompt(questions).then((answers) => {
-      let emp1 = new anything(answers.title, answers.name, answers.office, answers.id);
-      console.log(emp1.title, emp1.name);
-   });
+   addEmployee(title);
 }
+
+const questions = [
+   {
+      type: "text",
+      name: "title",
+      message: "What is your title?",
+   },
+
+   {
+      type: "text",
+      name: "name",
+      message: "What is your name?",
+   },
+   {
+      type: "text",
+      name: "office",
+      message: "What is your Office?",
+   },
+
+   {
+      type: "text",
+      name: "id",
+      message: "What is your ID?",
+   },
+];
+
+inquirer.prompt(questions).then((answers) => {
+   let emp1 = new anything(answers.title, answers.name, answers.office, answers.id);
+   console.log(emp1.title, emp1.name);
+});
